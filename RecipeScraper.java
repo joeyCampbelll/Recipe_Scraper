4396059b1e3f11f0f0774bada1e9c650a2453a1b
joeyCampbelll <joey.m.campbell@gmail.com>
2022-09-01 22:43:09 -0700

    can now retrieve each recipe link

--- RecipeScraper.java
+++ RecipeScraper.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.*;
 
 public class RecipeScraper {
@@ -14,12 +16,32 @@ public class RecipeScraper {
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-		//output to a file so its easy to mess with (you won’t be for your finished program)
-		FileOutputStream fs = new FileOutputStream("ioFiles\\tempOutput.txt");
-		PrintWriter pw = new PrintWriter(fs);
-		//response.body() is the html source code in a string  format. It outputs to a file so you 
-		// can see it easier right now, but you will ultimately want to just manipulate the strings a lot
-        pw.println(response.body()); 
-        pw.close();
+		// OLD CODE TO OUTPUT HTML RESPONSE TO FILE
+		
+		// //output to a file so its easy to mess with (you won’t be for your finished program)
+		// FileOutputStream fs = new FileOutputStream("ioFiles\\tempOutput.txt");
+		// PrintWriter pw = new PrintWriter(fs);
+		// //response.body() is the html source code in a string  format. It outputs to a file so you 
+		// // can see it easier right now, but you will ultimately want to just manipulate the strings a lot
+        // pw.println(response.body());
+        // pw.close();
+
+		// ===================
+
+		String htmlText = response.body();
+
+		ArrayList<String> recipeLinks = new ArrayList<String>();
+
+		Scanner stringReader = new Scanner(htmlText);
+
+		while (stringReader.hasNext()) {
+			String result = stringReader.nextLine();
+			if (result.startsWith("<a class=\"thumb-link\" href=\"https://www.surlatable.com/")) {
+				// System.out.println(result);
+				result = result.substring(28);
+				result = result.split("\"")[0];
+				System.out.println(result);
+			}
+		}
     }
 }
